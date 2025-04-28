63ad2f143c03194a26f9c819571209035ac919f3
junvalentine <lehongdaiminh@gmail.com>
2025-04-29 01:02:13 +0700

    add src

--- zklogin/build.sh
+++ zklogin/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
 echo "1. clearing files to rebuild"
-rm -rf ./target && mkdir ./target
+rm -rf ./out && mkdir ./out
 
 echo "2. compiling circuit to snarkjs..."
-circom circuit.circom --r1cs --wasm --sym --output=target
+circom circuit.circom --r1cs --wasm --sym --output=out
 
 echo "3. groth16 setup"
-snarkjs groth16 setup target/circuit.r1cs pot14_final.ptau target/circuit_0001.zkey
+snarkjs groth16 setup out/main.r1cs pot14_final.ptau out/circuit_0001.zkey
 
 echo "4. export verification key"
 snarkjs zkey export verificationkey target/circuit_0001.zkey target/verification_key.json
